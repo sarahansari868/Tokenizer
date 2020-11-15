bed943ab14ac3088de1695747975bd7119229163
Sarah Ansari <45130050+sarahansari868@users.noreply.github.com>
2020-11-15 13:21:53 -0500

    main program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Tokenizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            using (var sr = new StreamReader("Test_Cases/input.txt"))
+            {
+                input = sr.ReadToEnd();
+            }
+
+            if(input.Length > 0)
+            {
+
+                var tokenizer = new TokenOps.Tokenizer();
+                var tokenSeq = tokenizer.Tokenize(input);
+
+
+                var json = JsonConvert.SerializeObject(tokenSeq);
+
+                Console.WriteLine("Input: \n" + input);
+
+                Console.WriteLine("Output: \n" + json);
+               
+
+            }
+        }
+    }
+}
