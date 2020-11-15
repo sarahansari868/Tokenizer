5fb0767d123a2b2ebdf45b1932ded97c9af5321c
Sarah Ansari <45130050+sarahansari868@users.noreply.github.com>
2020-11-15 13:23:07 -0500

    Token type def as enum

---
+++ Lib/TokenType.cs
@@ -0,0 +1,25 @@
+﻿public enum TokenType
+{
+    PlIdentifiers,
+    JString,
+    CInteger,
+    CChar,
+    CFloat,
+    AdditionOp,
+    AssignmentOp,
+    SubtractionOp,
+    DivisionOp,
+    MultiplicationOp,
+    IncrementOp,
+    DecrementOp,
+    ModuloOp,
+    LAndOp,
+    LOrOp,
+    LNot,
+    OpenCodeBlock,
+    CloseCodeBlock,
+    OpenFnParam,
+    CloseFnParam,
+    SequenceTerminator,
+    LineTerminator
+}
